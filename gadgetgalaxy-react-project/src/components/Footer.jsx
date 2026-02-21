875338b16595341c90f9da9985a16d2847fb4672
Maryam Arif <bibi.maryam.arif@gmail.com>
2026-02-21 23:00:11 +0430

    feat: Add responsive footer code on Footer component
    Add responsive footer code on Footer component
    
    - Create responsive footer by using bootstrap class

---
+++ gadgetgalaxy-react-project/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <div className="container text-center">
+      <div className="row">
+        <div className="col">
+          <footer className="footer">
+            Codded By Maryam Arif | &#x00A9; 2026
+          </footer>
+        </div>
+      </div>
+    </div>
+  );
+}
