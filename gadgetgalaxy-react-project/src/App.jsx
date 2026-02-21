405858ef61c938399b81aa5531fb974fab4327dc
Maryam Arif <bibi.maryam.arif@gmail.com>
2026-02-21 23:48:52 +0430

    feat: Define the Routs into App.sx
    Define the Routs into App.sx
    
    - define the routes by using <Routes /> and <Route /> tags from react-router-dom
    - Define each Route with it is path and element
    - Define nested Route for passing the Outlet

--- gadgetgalaxy-react-project/src/App.jsx
+++ gadgetgalaxy-react-project/src/App.jsx
@@ -1,5 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import GeneralLayout from "./layouts/GeneralLayout";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import PageNotFound404 from "./pages/PageNotFound404";
+import ProductDetails from "./pages/ProductDetails";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route element={<GeneralLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
+        </Route>
+        <Route path="*" element={<PageNotFound404 />}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
